5d518d3d99dcf7d21790643f984a1fdf33ec3c56
EvanPavley <github email address>
2019-01-29 12:53:39 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
 
+function nestedTarget(){
+  const n = getFirstSelector('#nested')
+  return n.querySelector('.target')
+}
+
+function increaseRankBy(n){
+  const lis = document.querySelectorAll('ul.ranked-list li')
+  for (let i = 0; i < lis.length; i++) {
+    // lis[i].innerHTML = (i + 1 + n).toString();
+    const liNum = parseInt(lis[i].innerHTML);
+    lis[i].innerHTML = (liNum + n).toString();
+  }
+}
+
+function deepestChild() {
+  const gn = document.querySelector('#grand-node')
+  const divs = gn.getElementsByTagName('div')
+  const deepest = divs[divs.length - 1];
+  return deepest
+}
